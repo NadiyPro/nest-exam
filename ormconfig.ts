51d05c6e3c4fb4ae9f4cbb45cc50902ae39af437
NadiyPro <siroviyn13@gmail.com>
2024-11-23 19:07:52 +0200

    create

--- ormconfig.ts
+++ ormconfig.ts
@@ -20,13 +20,27 @@ export default new DataSource({
   password: config.password,
   database: config.name,
   entities: [
-    path.join(process.cwd(), 'src', 'database', 'entities', '*.entity.ts'),
+    path.join(
+      process.cwd(),
+      'src',
+      'infrastructure',
+      'postgres',
+      'entities',
+      '*.entity.ts',
+    ),
   ],
   // Шлях до файлів ентіті напряму до ts файлів, бо тут ми хочемо
   // ранити наші міграції НЕ піднімаючи аплікейшн (app)
   // Ентіті - описує структуру таблиці в БД, це типу як моделі
   migrations: [
-    path.join(process.cwd(), 'src', 'database', 'migrations', '*.ts'),
+    path.join(
+      process.cwd(),
+      'src',
+      'infrastructure',
+      'postgres',
+      'migrations',
+      '*.ts',
+    ),
   ],
   // Шлях до файлів міграцій
   synchronize: false,
