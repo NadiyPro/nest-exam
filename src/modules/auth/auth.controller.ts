6b227246ab3dc7b4b0c12c623b8fdf63b9995610
NadiyPro <siroviyn13@gmail.com>
2024-11-23 17:30:48 +0200

    create

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,61 +1,61 @@
-// import { Body, Controller, Post, UseGuards } from '@nestjs/common';
-// import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
-//
-// import { CurrentUser } from './decorators/current_user.decorator';
-// import { SkipAuth } from './decorators/skip_auth.decorator';
-// import { Jwt_refreshGuard } from './guards/jwt_refresh.guard';
-// import { LoginReqDto } from './models/dto/req/login';
-// import { RegistrationReqDto } from './models/dto/req/registration.req.dto';
-// import { AuthResDto } from './models/dto/res/auth.res.dto';
-// import { TokenPairResDto } from './models/dto/res/token_pair.res.dto';
-// import { IUserData } from './models/interfaces/user_data.interface';
-// import { AuthService } from './services/auth.service';
-//
-// @ApiTags('Auth')
-// @Controller('auth')
-// export class AuthController {
-//   constructor(private readonly authService: AuthService) {}
-//
-//   @SkipAuth()
-//   @Post('registration')
-//   public async registration(
-//     @Body() dto: RegistrationReqDto,
-//   ): Promise<AuthResDto> {
-//     return await this.authService.signUp(dto);
-//   }
-//   // singUp - тут логінація вконується (перший вхід з хешуванням паролю)
-//
-//   @SkipAuth()
-//   @Post('login')
-//   public async login(@Body() dto: LoginReqDto): Promise<AuthResDto> {
-//     return await this.authService.signIn(dto);
-//   }
-//   // signIn - тут проходить аутентифікація,
-//   // перевірка чи існує в нас такий юзер з таким то паролем,
-//   // якщо існує то генеруємо нову пару токенів
-//   // (повторний вхід, перевірка паролю, ат видача нової пари токенів)
-//
-//   @ApiBearerAuth()
-//   @Post('sign-out')
-//   public async signOut(@CurrentUser() userData: IUserData): Promise<void> {
-//     return await this.authService.signOut(userData);
-//   }
-//   // хочемо переходячи по даному шляху, видалят токени
-//   // які належать конкретному юзеру userData
-//   // userData містить в собі userId, deviceId, email
-//
-//   @SkipAuth()
-//   @ApiBearerAuth()
-//   @UseGuards(Jwt_refreshGuard)
-//   @Post('refresh')
-//   public async refresh(
-//     @CurrentUser() userData: IUserData,
-//   ): Promise<TokenPairResDto> {
-//     return await this.authService.refresh(userData);
-//   }
-//   // хочемо отримати нову пару токенів accessToken і refreshToken
-//   // перевіряємо чи в нас є юзер з таким паролем, якщо є генеруємо нові токени
-//   // видаляємо стару пару токенів
-//   // зберігаємо accessToken в кеш (Redis)
-//   // зберігаємо refreshToken в БД
-// }
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+
+import { CurrentUser } from './decorators/current_user.decorator';
+import { SkipAuth } from './decorators/skip_auth.decorator';
+import { Jwt_refreshGuard } from './guards/jwt_refresh.guard';
+import { LoginReqDto } from './models/dto/req/login.req.dto';
+import { RegistrationReqDto } from './models/dto/req/registration.req.dto';
+import { AuthResDto } from './models/dto/res/auth.res.dto';
+import { TokenPairResDto } from './models/dto/res/token_pair.res.dto';
+import { IUserData } from './models/interfaces/user_data.interface';
+import { AuthService } from './services/auth.service';
+
+@ApiTags('Auth')
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @SkipAuth()
+  @Post('registration')
+  public async registration(
+    @Body() dto: RegistrationReqDto,
+  ): Promise<AuthResDto> {
+    return await this.authService.signUp(dto);
+  }
+  // singUp - тут логінація вконується (перший вхід з хешуванням паролю)
+
+  @SkipAuth()
+  @Post('login')
+  public async login(@Body() dto: LoginReqDto): Promise<AuthResDto> {
+    return await this.authService.signIn(dto);
+  }
+  // signIn - тут проходить аутентифікація,
+  // перевірка чи існує в нас такий юзер з таким то паролем,
+  // якщо існує то генеруємо нову пару токенів
+  // (повторний вхід, перевірка паролю, ат видача нової пари токенів)
+
+  @ApiBearerAuth()
+  @Post('sign-out')
+  public async signOut(@CurrentUser() userData: IUserData): Promise<void> {
+    return await this.authService.signOut(userData);
+  }
+  // хочемо переходячи по даному шляху, видалят токени
+  // які належать конкретному юзеру userData
+  // userData містить в собі userId, deviceId, email
+
+  @SkipAuth()
+  @ApiBearerAuth()
+  @UseGuards(Jwt_refreshGuard)
+  @Post('refresh')
+  public async refresh(
+    @CurrentUser() userData: IUserData,
+  ): Promise<TokenPairResDto> {
+    return await this.authService.refresh(userData);
+  }
+  // хочемо отримати нову пару токенів accessToken і refreshToken
+  // перевіряємо чи в нас є юзер з таким паролем, якщо є генеруємо нові токени
+  // видаляємо стару пару токенів
+  // зберігаємо accessToken в кеш (Redis)
+  // зберігаємо refreshToken в БД
+}
--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtModule } from '@nestjs/jwt';
 
-// import { Jwt_refreshGuard } from './guards/jwt_refresh.guard';
-// import { AuthService } from './services/auth.service';
-// import { AuthCacheService } from './services/auth-cache-service';
-// import { TokenService } from './services/token.service';
 import { RedisModule } from '../../infrastructure/redis/redis.module';
+import { AuthController } from './auth.controller';
 import { Jwt_accessGuard } from './guards/jwt_access.guard';
+import { Jwt_refreshGuard } from './guards/jwt_refresh.guard';
+import { AuthService } from './services/auth.service';
+import { AuthCacheService } from './services/auth-cache.service';
+import { TokenService } from './services/token.service';
 
 @Module({
   imports: [JwtModule, RedisModule],
@@ -17,10 +18,10 @@ import { Jwt_accessGuard } from './guards/jwt_access.guard';
       provide: APP_GUARD,
       useClass: Jwt_accessGuard,
     },
-    // Jwt_refreshGuard,
-    // AuthService,
-    // AuthCacheService,
-    // TokenService,
+    Jwt_refreshGuard,
+    AuthService,
+    AuthCacheService,
+    TokenService,
   ],
   exports: [],
 })
