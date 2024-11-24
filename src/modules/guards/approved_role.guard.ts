35a3ab2c804d04fd95c88ca5081a143b6ea45b96
NadiyPro <siroviyn13@gmail.com>
2024-11-24 14:52:16 +0200

    create

--- src/modules/guards/approved_role.guard.ts
+++ src/modules/guards/approved_role.guard.ts
@@ -68,22 +68,9 @@ import { RoleTypeEnum } from '../users/enums/RoleType.enum';
 @Injectable()
 export class ApprovedRoleGuard implements CanActivate {
   constructor(private readonly reflector: Reflector) {}
-
-  // canActivate(context: ExecutionContext): boolean {
-  //   const roles = this.reflector.get<RoleTypeEnum[]>(
-  //     'roles',
-  //     context.getHandler(),
-  //   );
-  //   if (!roles) return true;
-  //
-  //   const request = context.switchToHttp().getRequest();
-  //   const user = request.user;
-  //
-  //   return roles.includes(user.role);
-  // }
   canActivate(context: ExecutionContext): boolean {
     const roles = this.reflector.get<RoleTypeEnum[]>(
-      'role',
+      'roles',
       context.getHandler(),
     );
     if (!roles) return true;
