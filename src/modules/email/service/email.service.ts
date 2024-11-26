918574c4144081d0fe529e83f5d72491b5a558cb
NadiyPro <siroviyn13@gmail.com>
2024-11-26 16:28:57 +0200

    create

--- src/modules/email/service/email.service.ts
+++ src/modules/email/service/email.service.ts
@@ -1,4 +1,3 @@
-// import { EmailTypeToPayload } from '../types/email-type-to-payload.type';
 import { ConfigService } from '@nestjs/config/dist/config.service';
 import { Transporter } from 'nodemailer';
 import * as nodemailer from 'nodemailer';
@@ -6,9 +5,9 @@ import * as hbs from 'nodemailer-express-handlebars';
 import * as path from 'path';
 
 import { Config } from '../../../configs/config.type';
+import { emailConstants } from '../constants/email.constants';
 import { EmailTypeEnum } from '../enums/email.enum';
 import { EmailTypeToPayload } from '../types/type_to_payload.type';
-import { emailConstants } from '../constants/email.constants';
 
 export class EmailService {
   private transporter: Transporter;
@@ -64,11 +63,6 @@ export class EmailService {
     context: EmailTypeToPayload[T],
   ): Promise<void> {
     const { subject, template } = emailConstants[type];
-
-    // context['frontUrl'] = configService.APP_FRONT_URL;
-    // тут ми вказуємо, що в нас буде змінна frontUrl,
-    // яка буде дорівнювати нашому хост (http://localhost:3001)
-    // *хост беремо з .env
     const options = { to, subject, template, context };
     await this.transporter.sendMail(options);
   }
