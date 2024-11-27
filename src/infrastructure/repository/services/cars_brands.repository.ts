7035bfa58a71335964c03db938a4da3635b063f3
NadiyPro <siroviyn13@gmail.com>
2024-11-27 23:26:30 +0200

    create

--- src/infrastructure/repository/services/cars_brands.repository.ts
+++ src/infrastructure/repository/services/cars_brands.repository.ts
@@ -46,29 +46,3 @@ export class CarsBrandsRepository extends Repository<CarsBrandsEntity> {
     return await qb.getManyAndCount();
   }
 }
-// public async findAll(
-//   userData: IUserData,
-//   query: ListArticleQueryDto,
-// ): Promise<[CarsBrandsEntity[], number]> {
-//   const qb = this.createQueryBuilder('article');
-//   qb.leftJoinAndSelect('article.tags', 'tag');
-//   qb.leftJoinAndSelect('article.user', 'user');
-//   qb.leftJoinAndSelect(
-//     'user.followings',
-//     'following',
-//     'following.follower_id = :userId',
-//   );
-//   qb.leftJoinAndSelect('article.likes', 'like', 'like.user_id = :userId');
-//   qb.setParameter('userId', userData.userId);
-//
-//   if (query.search) {
-//     qb.andWhere('CONCAT(article.title, article.description) ILIKE :search');
-//     qb.setParameter('search', `%${query.search}%`);
-//   }
-//   if (query.tag) {
-//     qb.andWhere('tag.name = :tag', { tag: query.tag });
-//   }
-//   qb.take(query.limit);
-//   qb.skip(query.offset);
-//   return await qb.getManyAndCount();
-// }
