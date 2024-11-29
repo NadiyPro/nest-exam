61fb04fbb06a59792266145d76765be992f7af62
NadiyPro <siroviyn13@gmail.com>
2024-11-29 18:17:31 +0200

    create

--- src/modules/advertisement/advertisement.module.ts
+++ src/modules/advertisement/advertisement.module.ts
@@ -1,16 +1,22 @@
 import { Module } from '@nestjs/common';
 
 import { AuthModule } from '../auth/auth.module';
+import { CarsModule } from '../cars/cars.module';
 import { EmailModule } from '../email/email.module';
 import { FileStorageModule } from '../file-storage/file-storage.module';
 import { UsersModule } from '../users/users.module';
 import { AvertisementController } from './advertisement.controller';
 import { AdvertisementJSONService } from './advertisementJSON/service/advertisementJSON.service';
 import { AdvertisementService } from './service/advertisement.service';
-import { CarsModule } from '../cars/cars.module';
 
 @Module({
-  imports: [UsersModule, AuthModule, FileStorageModule, EmailModule, CarsModule],
+  imports: [
+    UsersModule,
+    AuthModule,
+    FileStorageModule,
+    EmailModule,
+    CarsModule,
+  ],
   controllers: [AvertisementController],
   providers: [AdvertisementService, AdvertisementJSONService],
   exports: [AdvertisementService, AdvertisementJSONService],
