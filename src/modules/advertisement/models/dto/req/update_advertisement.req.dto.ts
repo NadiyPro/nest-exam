e06f474d9412913b7f1d55e9603b6a0adee46b6b
NadiyPro <siroviyn13@gmail.com>
2024-12-01 23:30:52 +0200

    create

--- src/modules/advertisement/models/dto/req/update_advertisement.req.dto.ts
+++ src/modules/advertisement/models/dto/req/update_advertisement.req.dto.ts
@@ -3,7 +3,6 @@ import { PickType } from '@nestjs/swagger';
 import { AdvertisementResDto } from '../res/advertisement.res.dto';
 
 export class UpdateAdMeReqDto extends PickType(AdvertisementResDto, [
-  'id',
   'price',
   'original_currency',
   'text_advertisement',
