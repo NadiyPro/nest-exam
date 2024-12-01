64224f6eff18da77f05726cc3519985adc5e7088
NadiyPro <siroviyn13@gmail.com>
2024-12-01 14:03:34 +0200

    create

--- src/modules/advertisement/advertisement.controller.ts
+++ src/modules/advertisement/advertisement.controller.ts
@@ -5,6 +5,7 @@ import {
   Get,
   Param,
   ParseUUIDPipe,
+  Patch,
   Post,
   UploadedFile,
   UseGuards,
@@ -21,6 +22,8 @@ import {
 import { ApiFile } from '../../common/decorators/api-file.decorator';
 import { CurrentUser } from '../auth/decorators/current_user.decorator';
 import { IUserData } from '../auth/models/interfaces/user_data.interface';
+import { CreateCarsReqDto } from '../cars/models/dto/req/create_cars.req.dto';
+import { CarsResDto } from '../cars/models/dto/res/cars.res.dto';
 import { CarsService } from '../cars/service/cars.service';
 import { EmailService } from '../email/service/email.service';
 import { ApprovedRoleGuard } from '../guards/approved_role.guard';
@@ -51,7 +54,9 @@ export class AvertisementController {
       'Увага! Продавець з типом акаунту basic може завантажити лише одне оголошення, ' +
       'для акаунту premium - кількість оголошень не обмежена. ' +
       '*Менеджеру надходить лист на пошту для перевірки оголошення, ' +
-      'якщо в ньому потенційно є некоректний/нецензурний текст.' +
+      'якщо в ньому потенційно є некоректний/нецензурний текст. ' +
+      'При цьому, на ча перевірки оголошення переходить в статус - pending. ' +
+      'Після перевірки тексту оголошення менеджером, статус зміниться на active або inactive відповідно.' +
       'Доступно для ролей: admin, manager, seller',
   })
   @ApiBearerAuth()
@@ -103,6 +108,27 @@ export class AvertisementController {
     );
   }
 
+  @ApiBearerAuth()
+  @UseGuards(ApprovedRoleGuard)
+  @Role([RoleTypeEnum.ADMIN, RoleTypeEnum.MANAGER])
+  @ApiOperation({
+    summary: 'Для редагування свого оголошення по id. ',
+    description:
+      'Користувач може відредагувати своє оголошення по id в своєму оголошені: текст, суму та валюту.' +
+      'Увага! Якщо користувач редагує повідомлення три рази, ' +
+      'то менеджеру надходить лист на пошту, про необхідність перевірити дане оголошення. ' +
+      'На час перевірки, оголошення змінює свій статус на - pending. ' +
+      'Після перевірки тексту оголошення менеджером, статус зміниться на active або inactive відповідно.' +
+      'Доступно для ролей: admin, manager',
+  })
+  @Patch(':carsBrandsId')
+  public async updateCars(
+    @Param('brands_id', ParseUUIDPipe) brands_id: string,
+    @Body() dto: CreateCarsReqDto,
+  ): Promise<CarsResDto> {
+    return await this.carsService.updateCars(brands_id, dto);
+  }
+
   @ApiOperation({
     summary: 'Для виванатження оголошення по його id',
     description:
@@ -120,6 +146,31 @@ export class AvertisementController {
       advertisementId,
     );
   }
+
+  // @ApiBearerAuth()
+  // @UseGuards(ApprovedRoleGuard)
+  // @Role([RoleTypeEnum.ADMIN, RoleTypeEnum.MANAGER])
+  // @ApiOperation({
+  //   summary:
+  //     'Для оновлення конкретного запису про автомобіль (бренд та модель) ' +
+  //     'по id бренду до якого підвязана конкретна модель',
+  //   description:
+  //     'Користувач може оновити запис про автомобіль (бренд та модель) ' +
+  //     'Вказавши саме brands_id до якого підвязана конкретна модель. ' +
+  //     'Даний brands_id можна взяти як варіант з cars/all/cars_brands.' +
+  //     'Увага! Для оновлення треба: ' +
+  //     '1.здійснити пошук потрібного нам автомобіля по brands_id; ' +
+  //     '2.вказати обовязково два аргументи brands_name та models_name' +
+  //     'Наприклад якщо була допущена помилка в записі та інше.' +
+  //     'Доступно для ролей: admin, manager',
+  // })
+  // @Patch(':carsBrandsId')
+  // public async updateCars(
+  //   @Param('brands_id', ParseUUIDPipe) brands_id: string,
+  //   @Body() dto: CreateCarsReqDto,
+  // ): Promise<CarsResDto> {
+  //   return await this.carsService.updateCars(brands_id, dto);
+  // }
 
   @ApiOperation({
     summary: 'Для виванатження оголошення по його id',
